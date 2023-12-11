7f0f59c5a8c9ffa2bc7ed8756fe60d2cec6a30de
SergeiM <mihailovsv@mail.ru>
2023-12-11 22:05:22 +0300

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,13 @@
+public class Dog extends Animal{
+
+    private int speed;
+    private String voice = "meow";
+    public fed 
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    public void feed(){
+        System.out.println();
+    }
+}
